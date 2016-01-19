64d8320b913bf1c81581fc2c1aa632ca000b7db8
Jason Teplitz <jteplitz602@gmail.com>
2016-01-19 04:56:45 -0800

    dump to file every 50 requests

--- master.js
+++ master.js
@@ -1,9 +1,10 @@
 var Pool = require('phantomjs-pool').Pool;
-var urls = require('./2010.1.json');
+var urls = require('./2010.3.json');
 var fs = require('fs');
 var allMatches = [];
 var numErrors = 0;
 var numDone = 0;
+var filesWritten = 0;
 var NUM_WORKERS = 10;
 var outFile = process.argv[2];
 console.log("writing to", outFile);
@@ -25,11 +26,18 @@ function jobCallback(job, worker, index) {
         console.log("Done processing", url);
         allMatches = allMatches.concat(matches);
       }
-      if (numDone >= urls.length - 1) {
-        fs.writeFileSync(outFile || "out.json", JSON.stringify(allMatches));
+      if (numDone >= urls.length - 1 || numDone % 50 === 0) {
+        writeFile(allMatches);
       }
     });
   }
+}
+
+function writeFile(matches) {
+  var name = new Date().getTime() + "-" + filesWritten + (outFile || "out.json");
+  console.log("Dumping to", name);
+  fs.writeFileSync(name, JSON.stringify(allMatches));
+  filesWritten++;
 }
 
 var pool = new Pool({
